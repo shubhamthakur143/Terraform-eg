e6a06778afae3069f6f00cba2058c99bc72f369d
Shubham Thakur <155795907+shubhamthakur143@users.noreply.github.com>
2026-06-19 15:41:55 +0530

    Create Variable.tf
    Variable for ASg

---
+++ Variable.tf
@@ -0,0 +1,19 @@
+variable "image" {
+  default = "ami-0b6d9d3d33ba97d99"
+}
+
+variable "type" {
+  default = "t3.micro"
+}
+
+variable "key" {
+  default = "rcb"
+}
+
+variable "sg" {
+  default = "sg-076b489157e9837d0"
+}
+
+variable "vpc" {
+  default = "vpc-0ebec8c800d38a679"
+}
